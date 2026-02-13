9ebdfff390afa4ca6f8e0ae3b4a2f8bc5ae42c3e
NathanWMartins <nathanwillmartins@gmail.com>
2026-02-13 12:23:55 -0300

    fix: correção do app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,8 @@ import { AuthModule } from './module/auth.module';
 import { AppController } from 'src/app.controller';
 import { AppService } from 'src/app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/user/user.entity';
 import { ConfigModule } from '@nestjs/config';
-import { Local } from './local/local.entity';
 import { LocalModule } from './module/local.module';
-import { HorarioFuncionamento } from './agendamentos/horario-funcionamento.entity';
 import { UploadsModule } from './module/uploads.module';
 import { AgendamentosModule } from './module/agendamentos.module';
 import { LocadoresModule } from './module/locadores.module';
@@ -18,7 +15,7 @@ import { MensalidadesModule } from './module/mensalidades.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true }),    
     TypeOrmModule.forRoot({
       type: 'postgres',
       url: process.env.DATABASE_URL,
@@ -37,7 +34,7 @@ import { MensalidadesModule } from './module/mensalidades.module';
     UploadsModule,
     AuthModule,
     LocalModule,
-    MensalidadesModule
+    MensalidadesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
