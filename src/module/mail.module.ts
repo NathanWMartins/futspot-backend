650c01f42d6cabb640c9f8185cba8b2c3ad8f994
NathanWMartins <nathanwillmartins@gmail.com>
2026-02-23 16:23:36 -0300

    fix: logs para mail module

--- src/module/mail.module.ts
+++ src/module/mail.module.ts
@@ -8,23 +8,28 @@ console.log(process.env.MAIL_USER);
   imports: [
     MailerModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        transport: {
-          host: 'smtp.gmail.com',
-          port: 587,
-          secure: false,
-          auth: {
-            user: config.get<string>('MAIL_USER'),
-            pass: config.get<string>('MAIL_PASS'),
+      useFactory: (config: ConfigService) => {
+        console.log('MAIL_USER:', config.get('MAIL_USER'));
+        console.log('MAIL_PASS exists:', !!config.get('MAIL_PASS'));
+
+        return {
+          transport: {
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+              user: config.get<string>('MAIL_USER'),
+              pass: config.get<string>('MAIL_PASS'),
+            },
           },
-        },
-        defaults: {
-          from: `"FutSpot" <${config.get('MAIL_USER')}>`,
-        },
-      }),
+          defaults: {
+            from: `"FutSpot" <${config.get('MAIL_USER')}>`,
+          },
+        };
+      },
     }),
   ],
   providers: [MailService],
   exports: [MailService],
 })
-export class MailModule {}
+export class MailModule { }
